1c2730bf3d3862c35c33672747de9bf57617d32a
tvdtb <tvdtb>
2018-10-22 05:39:26 +0200

    bugfix mixed case (mysql)

--- src/test/resources/META-INF/sql/drop.sql
+++ src/test/resources/META-INF/sql/drop.sql
@@ -2,9 +2,9 @@ drop table sequences:
 drop sequence hibernate_sequence;
 drop table hibernate_sequence;
 
-drop table city;
-drop table postalcode;
-drop table state;
+drop table City;
+drop table PostalCode;
+drop table State;
 
 drop table TestEntity;
 drop table NamedEntity;
