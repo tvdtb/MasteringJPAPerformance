43bc16e8b69ca68023209c0acc43a36682ad0e1c
tvdtb <tvdtb>
2018-10-24 13:32:39 -0700

    bugfix

--- src/test/resources/META-INF/sql/create.sql
+++ src/test/resources/META-INF/sql/create.sql
@@ -1,7 +1,6 @@
 create table sequences          (name varchar(50) not null, value    bigint, primary key (name));
 create sequence hibernate_sequence;
 create table hibernate_sequence (name varchar(50) not null, next_val bigint, primary key (name));
-insert into hibernate_sequence(name, next_val) ('default', 10000);
 
 create table city       (id bigint not null, name varchar(255), state_id bigint, lorem varchar(255), primary key (id));
 create table postalcode (id bigint not null, code varchar(255), city_id bigint,  lorem varchar(255), primary key (id));
